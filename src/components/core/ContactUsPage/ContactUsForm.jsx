dbfd5a5b0a997b78cfd00a94586ba6148ef43b5a
kishan <kkseth412@gmail.com>
2023-08-07 21:27:37 +0530

    changes

--- src/components/core/ContactUsPage/ContactUsForm.jsx
+++ src/components/core/ContactUsPage/ContactUsForm.jsx
@@ -18,11 +18,11 @@ const ContactUsForm = () => {
     // console.log("Form Data - ", data)
     try {
       setLoading(true)
-      const res = await apiConnector(
-        "POST",
-        contactusEndpoint.CONTACT_US_API,
-        data
-      )
+      // const res = await apiConnector(
+      //   "POST",
+      //   contactusEndpoint.CONTACT_US_API,
+      //   data
+      // )
       // console.log("Email Res - ", res)
       setLoading(false)
     } catch (error) {
